3ea3e8c215c1f71e7c901ecb7899900a407f778b
Carlos Eduardo <cadu.010808@gmail.com>
2023-05-02 22:44:50 -0300

    function close modal adicionada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,97 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import Header from './Header'
-import exames from '../data/exame'
-import styles from './Home.module.css';
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import exames from "../data/exame";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  const [search, setSearch] = useState('');  //input value
-  const [listExam, setListExame] = useState([]);//recebe exames filtrados
-  const [boxOptions, setBoxOptions] = useState(false);//on/off modal de opcoes
-  const [examsSelectBox, setExamsSelectBox] = useState([])//Array de exames selecionados do boxOptions
-  
+  const [search, setSearch] = useState(""); //input value
+  const [listExam, setListExame] = useState([]); //recebe exames filtrados
+  const [boxOptions, setBoxOptions] = useState(false); //on/off modal de opcoes
+  const [examsSelectBox, setExamsSelectBox] = useState([]); //Array de exames selecionados do boxOptions
 
-
-  const allExames = exames
+  const allExames = exames;
 
   useEffect(() => {
-    const examSelect = allExames.filter(exam => {
+    const examSelect = allExames.filter((exam) => {
       return (
         exam.name.toLowerCase().includes(search) ||
         exam.nick.toLowerCase().includes(search)
-      )
-    })
-    setListExame(examSelect)
-    openModalBoxOptions()
-  }, [search])
-
+      );
+    });
+    openModalBoxOptions();
+    setListExame(examSelect);
+  }, [search]);
 
   function openModalBoxOptions() {
-    setBoxOptions(!boxOptions)
-    console.log(boxOptions)
+    setBoxOptions(!boxOptions);
   }
 
+    //closeModel boxOptions
+    useEffect(() => { 
+      window.addEventListener('click', (event) => {
+        if(event.target != boxOptions) {
+          setBoxOptions(!boxOptions)
+        }
+      })
+    }, [])
 
   function handleClickOnBox(event) {
-    setExamsSelectBox(examsSelectBox => [...examsSelectBox, event.innerHTML])
-    console.log(examsSelectBox)
+    setExamsSelectBox((examsSelectBox) => [...examsSelectBox, event.innerHTML]);
+    setBoxOptions(!boxOptions);
   }
+
 
   return (
     <>
       <Header />
-    <section className={styles.wrapperHome}>
-      <div className={styles.searchArea}>
 
-      <label>
-        Digite seus exames
+      <section className={styles.wrapperHome}>
+        <div className={styles.searchArea}>
+          <label>
+            Digite seus exames
+            <input
+              type="search"
+              name=""
+              id=""
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </label>
+          <button type="button" value="Enter" >
+            {" "}
+            Selecionar
+          </button>
+        </div>
 
-        <input type="search"
-             name="" 
-             id=""
-             onChange={(e) => setSearch(e.target.value)}
-             
-         />
-      </label> 
-      <button type="button"
-             value="Enter"
-             onClick={() => {}}
-              > Selecionar
-      </button>  
-     </div>    
-
-      {boxOptions ?
-       '' :
-     <div className={styles.examContainer}>
-       <div className={styles.examList}>
-           {listExam?.map((exam, index) => (
-
-                <li 
-                  key={index}
-                  onClick={(e) => handleClickOnBox(e.target)}>
-
+        {/*opcoes de exames clicaveis*/}
+        {boxOptions ? (
+          ""
+        ) : (
+          <div className={styles.examContainer}>
+            <div className={styles.examList}>
+              {listExam?.map((exam, index) => (
+                <li key={index} 
+                    onClick={(e) => handleClickOnBox(e.target)}>
                   {exam.name}
-
                 </li>
-            ))}
-       </div>
-     </div> 
-     }
+              ))}
+            </div>
+          </div>
+        )}
 
+        <div className={styles.separator} />
 
-     <hr></hr>
-     {examsSelectBox ? 
-        examsSelectBox.map((exam, index) => (
-          <ul key={index}>
-            <li>{exam}</li>
-          </ul>
-        ))
-      : '' }
+          
+        {examsSelectBox
+          ? 
+          examsSelectBox.map((exam, index) => (
+              <ul key={index} className={styles.resultExames}>
+                <li>{exam}</li>
+              </ul>
+            ))  
+            
+            : ""}
+      </section>
+    </>
+  );
+};
 
-     
-    </section>
-
-  </>
-  )
-}
-
-export default Home
+export default Home;
